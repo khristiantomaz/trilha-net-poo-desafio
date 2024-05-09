d97fd47482bda3fe57074d023741c06324e06c30
Khristian-Tomaz <khristiantomaz@icloud.com>
2024-05-08 23:04:42 -0300

    modified:   DesafioPOO.csproj 	modified:   Models/Iphone.cs 	modified:   Models/Nokia.cs 	modified:   Models/Smartphone.cs 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.Linq.Expressions;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia:");
+ Smartphone nokia = new  Nokia(numero:"12343", modelo: "Modelo 1", imei: "1111111111111", memoria: 64);
+ nokia.Ligar();
+ nokia.InstalarAplicativo("Whatsapp");
+
+ System.Console.WriteLine("\n");
+
+ System.Console.WriteLine("Smartphone Iphone"); 
+  Smartphone iphone = new  Iphone(numero:"112312", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+  iphone.ReceberLigacao();
+  iphone.InstalarAplicativo("Telegram");
+
